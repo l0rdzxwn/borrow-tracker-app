8d7dc2ba6c10961967904324aad975273d6d601d
lordz <lordzierwin17@gmail.com>
2026-02-16 01:46:37 +0800

    fix(Item Display Feature): Fixed Items not getting sorted per user

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Borrow Tracker',
       home: StreamProvider<FirebaseUser?>.value(
         initialData: null,
         value: AuthServices().userID,
